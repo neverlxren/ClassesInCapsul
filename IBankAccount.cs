71b140227639088476ef2053ed3cd621fb568844
neverlxren <17ivanyden@gmail.com>
2026-06-17 00:02:58 +0200

    menu code done

--- IBankAccount.cs
+++ IBankAccount.cs
@@ -1,6 +1,17 @@
+
 namespace ClassesInCapsul;
 
 public interface IBankAccount
 {
-    
+         bool InsertCard(string cardNumber);
+         bool VerifyPin(string pin);
+         
+         bool Withdraw(decimal amount);
+         bool Deposit(decimal amount);
+         bool Transfer(string targetAccount, decimal amount);
+         void ChangePin(string oldPin, string newPin);
+        /* List<Transaction> GetMiniStatement();*/
+         void PrintReceipt();
+         void Logout();
+
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassesInCapsul;
+using ExampleOOP;
+
+
+BankAccount acc1 = new BankAccount("Volodymyr Zelenskyy");
+
+acc1.ShowMenu();
